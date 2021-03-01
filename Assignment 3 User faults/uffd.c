1f08dc3aca23c349017967353f07406eeb563dd2
Pratik Nagelia <pratik0204@gmail.com>
2021-03-01 22:24:18 +0000

    Implement part 1 exchange and mmap

---
+++ Assignment 3 User faults/uffd.c
@@ -0,0 +1,311 @@
+/* userfaultfd_demo.c
+
+   Licensed under the GNU General Public License version 2 or later.
+*/
+#define _GNU_SOURCE
+#include <sys/types.h>
+#include <stdio.h>
+#include <linux/userfaultfd.h>
+#include <pthread.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <poll.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/syscall.h>
+#include <sys/ioctl.h>
+#include <poll.h>
+
+#define errExit(msg)    do { perror(msg); exit(EXIT_FAILURE);	\
+	} while (0)
+
+static int page_size;
+
+static void *
+fault_handler_thread(void *arg)
+{
+	static struct uffd_msg msg;   /* Data read from userfaultfd */
+	static int fault_cnt = 0;     /* Number of faults so far handled */
+	long uffd;                    /* userfaultfd file descriptor */
+	static char *page = NULL;
+	struct uffdio_copy uffdio_copy;
+	ssize_t nread;
+
+	uffd = (long) arg;
+
+	/* [H1]
+	 * Explain following in here.
+	 */
+	if (page == NULL) {
+		page = mmap(NULL, page_size, PROT_READ | PROT_WRITE,
+			    MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+		if (page == MAP_FAILED)
+			errExit("mmap");
+	}
+
+	/* [H2]
+	 * Explain following in here.
+	 */
+	for (;;) {
+
+		/* See what poll() tells us about the userfaultfd */
+
+		struct pollfd pollfd;
+		int nready;
+
+		/* [H3]
+		 * Explain following in here.
+		 */
+		pollfd.fd = uffd;
+		pollfd.events = POLLIN;
+		nready = poll(&pollfd, 1, -1);
+		if (nready == -1)
+			errExit("poll");
+
+		printf("\nfault_handler_thread():\n");
+		printf("    poll() returns: nready = %d; "
+                       "POLLIN = %d; POLLERR = %d\n", nready,
+                       (pollfd.revents & POLLIN) != 0,
+                       (pollfd.revents & POLLERR) != 0);
+
+		/* [H4]
+		 * Explain following in here.
+		 */
+		nread = read(uffd, &msg, sizeof(msg));
+		if (nread == 0) {
+			printf("EOF on userfaultfd!\n");
+			exit(EXIT_FAILURE);
+		}
+
+		if (nread == -1)
+			errExit("read");
+
+		/* [H5]
+		 * Explain following in here.
+		 */
+		if (msg.event != UFFD_EVENT_PAGEFAULT) {
+			fprintf(stderr, "Unexpected event on userfaultfd\n");
+			exit(EXIT_FAILURE);
+		}
+
+		/* [H6]
+		 * Explain following in here.
+		 */
+		printf("    UFFD_EVENT_PAGEFAULT event: ");
+		printf("flags = %llx; ", msg.arg.pagefault.flags);
+		printf("address = %llx\n", msg.arg.pagefault.address);
+
+		/* [H7]
+		 * Explain following in here.
+		 */
+		memset(page, 'A' + fault_cnt % 20, page_size);
+		fault_cnt++;
+
+		/* [H8]
+		 * Explain following in here.
+		 */
+		uffdio_copy.src = (unsigned long) page;
+		uffdio_copy.dst = (unsigned long) msg.arg.pagefault.address &
+			~(page_size - 1);
+		uffdio_copy.len = page_size;
+		uffdio_copy.mode = 0;
+		uffdio_copy.copy = 0;
+
+		/* [H9]
+		 * Explain following in here.
+		 */
+		if (ioctl(uffd, UFFDIO_COPY, &uffdio_copy) == -1)
+			errExit("ioctl-UFFDIO_COPY");
+
+		/* [H10]
+		 * Explain following in here.
+		 */
+		printf("        (uffdio_copy.copy returned %lld)\n",
+                       uffdio_copy.copy);
+	}
+}
+
+int
+main(int argc, char *argv[])
+{
+	long uffd;          /* userfaultfd file descriptor */
+	char *addr;         /* Start of region handled by userfaultfd */
+	unsigned long len;  /* Length of region handled by userfaultfd */
+	pthread_t thr;      /* ID of thread that handles page faults */
+	struct uffdio_api uffdio_api;
+	struct uffdio_register uffdio_register;
+	int s;
+	int l;
+
+	/* [M1]
+	 * Explain following in here.
+	 */
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s num-pages\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	/* [M2]
+	 * Explain following in here.
+	 */
+	page_size = sysconf(_SC_PAGE_SIZE);
+	len = strtoul(argv[1], NULL, 0) * page_size;
+
+	/* [M3]
+	 * Explain following in here.
+	 */
+	uffd = syscall(__NR_userfaultfd, O_CLOEXEC | O_NONBLOCK);
+	if (uffd == -1)
+		errExit("userfaultfd");
+
+	/* [M4]
+	 * Explain following in here.
+	 */
+	uffdio_api.api = UFFD_API;
+	uffdio_api.features = 0;
+	if (ioctl(uffd, UFFDIO_API, &uffdio_api) == -1)
+		errExit("ioctl-UFFDIO_API");
+
+	/* [M5]
+	 * Explain following in here.
+	 */
+	addr = mmap(NULL, len, PROT_READ | PROT_WRITE,
+		    MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+	if (addr == MAP_FAILED)
+		errExit("mmap");
+
+	printf("Address returned by mmap() = %p\n", addr);
+
+	/* [M6]
+	 * Explain following in here.
+	 */
+	uffdio_register.range.start = (unsigned long) addr;
+	uffdio_register.range.len = len;
+	uffdio_register.mode = UFFDIO_REGISTER_MODE_MISSING;
+	if (ioctl(uffd, UFFDIO_REGISTER, &uffdio_register) == -1)
+		errExit("ioctl-UFFDIO_REGISTER");
+
+	/* [M7]
+	 * Explain following in here.
+	 */
+	s = pthread_create(&thr, NULL, fault_handler_thread, (void *) uffd);
+	if (s != 0) {
+		errno = s;
+		errExit("pthread_create");
+	}
+
+	/*
+	 * [U1]
+	 * Briefly explain the behavior of the output that corresponds with below section.
+	 */
+	printf("-----------------------------------------------------\n");
+	l = 0x0;
+	while (l < len) {
+		char c = addr[l];
+		printf("#1. Read address %p in main(): ", addr + l);
+		printf("%c\n", c);
+		l += 1024;
+	}
+
+	/*
+	 * [U2]
+	 * Briefly explain the behavior of the output that corresponds with below section.
+	 */
+	printf("-----------------------------------------------------\n");
+	l = 0x0;
+	while (l < len) {
+		char c = addr[l];
+		printf("#2. Read address %p in main(): ", addr + l);
+		printf("%c\n", c);
+		l += 1024;
+	}
+
+	/*
+	 * [U3]
+	 * Briefly explain the behavior of the output that corresponds with below section.
+	 */
+	printf("-----------------------------------------------------\n");
+	if (madvise(addr, len, MADV_DONTNEED)) {
+		errExit("fail to madvise");
+	}
+	l = 0x0;
+	while (l < len) {
+		char c = addr[l];
+		printf("#3. Read address %p in main(): ", addr + l);
+		printf("%c\n", c);
+		l += 1024;
+	}
+
+	/*
+	 * [U4]
+	 * Briefly explain the behavior of the output that corresponds with below section.
+	 */
+	printf("-----------------------------------------------------\n");
+	l = 0x0;
+	while (l < len) {
+		char c = addr[l];
+		printf("#4. Read address %p in main(): ", addr + l);
+		printf("%c\n", c);
+		l += 1024;
+	}
+
+	/*
+	 * [U5]
+	 * Briefly explain the behavior of the output that corresponds with below section.
+	 */
+	printf("-----------------------------------------------------\n");
+	if (madvise(addr, len, MADV_DONTNEED)) {
+		errExit("fail to madvise");
+	}
+	l = 0x0;
+	while (l < len) {
+		memset(addr+l, '@', 1024);
+		printf("#5. write address %p in main(): ", addr + l);
+		printf("%c\n", addr[l]);
+		l += 1024;
+	}
+
+	/*
+	 * [U6]
+	 * Briefly explain the behavior of the output that corresponds with below section.
+	 */
+	printf("-----------------------------------------------------\n");
+	l = 0x0;
+	while (l < len) {
+		char c = addr[l];
+		printf("#6. Read address %p in main(): ", addr + l);
+		printf("%c\n", c);
+		l += 1024;
+	}
+
+	/*
+	 * [U7]
+	 * Briefly explain the behavior of the output that corresponds with below section.
+	 */
+	printf("-----------------------------------------------------\n");
+	l = 0x0;
+	while (l < len) {
+		memset(addr+l, '^', 1024);
+		printf("#7. write address %p in main(): ", addr + l);
+		printf("%c\n", addr[l]);
+		l += 1024;
+	}
+
+	/*
+	 * [U8]
+	 * Briefly explain the behavior of the output that corresponds with below section.
+	 */
+	printf("-----------------------------------------------------\n");
+	l = 0x0;
+	while (l < len) {
+		char c = addr[l];
+		printf("#8. Read address %p in main(): ", addr + l);
+		printf("%c\n", c);
+		l += 1024;
+	}
+
+	exit(EXIT_SUCCESS);
+}
